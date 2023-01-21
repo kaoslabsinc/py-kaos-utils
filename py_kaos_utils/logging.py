b234924df265133f2247f898515b75419f9b9108
Keyvan Mosharraf <keyvanm@users.noreply.github.com>
2023-01-21 21:45:36 +0000

    Docstrings

--- py_kaos_utils/logging.py
+++ py_kaos_utils/logging.py
@@ -7,6 +7,17 @@ logger = logging.getLogger()
 
 @dataclass
 class ProgressLogger:
+    """
+    Logs the progress of a process by reporting the percentage of completion at a set number of checkpoints.
+
+    :param count_total: Total number of items to process
+    :type count_total: int
+    :param checkpoints: Number of checkpoints to report progress at (defaults to 10)
+    :type checkpoints: int
+    :param logger: Logger instance to use for reporting progress (defaults to the built-in logger)
+    :type logger: Logger
+    """
+
     count_total: int
     checkpoints: int = 10
     logger: Logger = logger
@@ -15,15 +26,30 @@ class ProgressLogger:
 
     @property
     def _each_checkpoint_length(self):
+        """
+        :return: Number of items per checkpoint
+        :rtype: int
+        """
         return (self.count_total // self.checkpoints) + 1
 
     def report_progress(self):
+        """
+        Report the progress of the process based on the current count_processed value
+        """
         checkpoint = self.count_processed // self._each_checkpoint_length
         if checkpoint > len(self._checkpoints_log):
             logger.info(f"{checkpoint / self.checkpoints:.0%} there: {self.count_processed}/{self.count_total}")
             self._checkpoints_log.append(self.count_processed)
 
     def log_progress(self, count_this_round, report=True):
+        """
+        Log the progress of the process
+
+        :param count_this_round: Number of items processed this round
+        :type count_this_round: int
+        :param report: Whether or not to report the progress (defaults to True)
+        :type report: bool
+        """
         self.count_processed += count_this_round
         if report:
             self.report_progress()
--- py_kaos_utils/pagination.py
+++ py_kaos_utils/pagination.py
@@ -3,9 +3,21 @@ from collections.abc import Generator
 from .typing import T
 
 
-def paginate_generator(generator: Generator[T, None, None], limit: int) -> Generator[list[T], None, None]:
+def paginate_generator(
+    generator: Generator[T, None, None],
+    limit: int
+) -> Generator[list[T], None, None]:
     """
-    If limit is zero (or negative for that matter), it returns the whole generator as a list
+    Paginate a generator by a given limit (page size).
+
+    :param generator: the generator to be paginated
+    :type generator: Generator[T, None, None]
+    :param limit: number of items per page
+    :type limit: int
+    :return: a generator of pages, where each page is a list of items
+    :rtype: Generator[list[T], None, None]
+    :Note:
+        If limit is zero (or negative for that matter), it returns the whole generator as a list
     """
     page: list[T] = []
     for item in generator:
