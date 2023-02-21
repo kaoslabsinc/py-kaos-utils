06f9eb60d040f20aa1bf00e42b64058a2c316879
Keyvan Mosharraf <keyvan@keyvanm.com>
2023-02-20 17:20:19 -0800

    more tests

--- tests/test_datetime.py
+++ tests/test_datetime.py
@@ -12,6 +12,10 @@ class TestBaseDatetimeWrapper:
         return dt.datetime(2022, 1, 1, 12, 30, 0)
 
     @pytest.fixture
+    def raw_dt__aware(self):
+        return dt.datetime(2022, 1, 1, 15, 30, 0, tzinfo=ZoneInfo('US/Eastern'))
+
+    @pytest.fixture
     def raw_tz_str(self):
         return 'US/Pacific'
 
@@ -42,3 +46,12 @@ class TestBaseDatetimeWrapper:
     def test_dt__raw_tz_set(self, raw_dt, raw_tz_str):
         dtw = DT(raw_dt, raw_tz_str)
         assert dtw.dt == dt.datetime(2022, 1, 1, 12, 30, tzinfo=ZoneInfo('US/Pacific'))
+
+    def test_dt_aware__raw_tz_set(self, raw_dt__aware, raw_tz_str):
+        dtw = DT(raw_dt__aware, raw_tz_str)
+        assert dtw.dt == dt.datetime(2022, 1, 1, 12, 30, tzinfo=ZoneInfo('US/Pacific'))
+
+    def test_two_dts(self, raw_dt, raw_dt__aware, raw_tz_str):
+        dtw1 = DT(raw_dt, raw_tz_str)
+        dtw2 = DT(raw_dt__aware, raw_tz_str)
+        assert dtw1.dt == dtw2.dt
